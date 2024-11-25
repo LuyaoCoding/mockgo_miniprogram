442a29152c8f608bd3fac347961553f31204aeaa
luyaoCoding <dxian0922@outlook.com>
2024-11-25 17:55:10 +0800

    封装天气签到卡片组件

---
+++ components/weather-card/weather-card.js
@@ -0,0 +1,25 @@
+// weather-card.js
+Component({
+  properties: {
+    location: {
+      type: String,
+      value: 'Messadine, Susah'
+    },
+    date: {
+      type: String,
+      value: 'March 13'
+    },
+    temperature: {
+      type: Number,
+      value: 23
+    }
+  },
+
+  data: {
+    // 组件内部数据
+  },
+
+  methods: {
+    // 组件内部方法
+  }
+})
